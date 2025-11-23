d89018d7d3f7451cc63ad40d07bc648efb9b4fb8
MatveyGucci <matveygucci@gmail.com>
2025-11-23 17:38:39 +0200

    sqlite -> postgresql

--- app/database.py
+++ app/database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mainproj.db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:SMue37m5R8@localhost:5432/mydatabase"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 def init_db():
-    import app.models 
+    import app.models
     Base.metadata.create_all(bind=engine)
+    print(f"Connected to DB: {SQLALCHEMY_DATABASE_URL}")
